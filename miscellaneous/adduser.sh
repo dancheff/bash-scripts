d98cd5e31c00304cfa56ee876a61589755c71319
Valentin Dancheff <vaalku@gmail.com>
2020-09-04 23:00:37 +0300

    Update adduser.sh

--- miscellaneous/adduser.sh
+++ miscellaneous/adduser.sh
@@ -12,7 +12,6 @@ if [ $(id -u) -eq 0 ]; then
 		echo "$username exists!"
 		exit 1
 	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 		useradd -m -p "$pass" "$username"
 		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
 	fi
